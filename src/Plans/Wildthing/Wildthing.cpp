cb1cf32a09d7efc0f63344aa4e362ddf62d9a156
Andrew-Cua <andrew@cuafamily.com>
2019-12-03 16:20:29 -0500

    made changes to Wildthing class for wirelessContrlr

--- src/Plans/Wildthing/Wildthing.cpp
+++ src/Plans/Wildthing/Wildthing.cpp
@@ -16,15 +16,20 @@ Wildthing::Wildthing()
 
 void Wildthing::setup()
 {
+    xbeeSerial.begin(9600);
+    while(!xbeeSerial){}
     Charibot::setup();
     pController         = new WiredController(JOYSTICK_X_PIN,JOYSTICK_Y_PIN);
     pLeftMotor          = new SparkMotor(LEFT_MOTOR_PIN, true);
     pRightMotor         = new SparkMotor(RIGHT_MOTOR_PIN,false);
     pDifferentialDrive  = new DifferentialDrive(*pLeftMotor,*pRightMotor);
     pControlPlan        = new SingleJoystickDrivePlan(*pDifferentialDrive,*pController);
+    pWirelessController = new WirelessController(xbeeSerial);
 }
 
 void Wildthing::loop()
 {
+    pWirelessController -> poll();
+    Serial.println(pWirelessController->getY);
     Charibot::loop();
 }
--- src/Plans/Wildthing/Wildthing.h
+++ src/Plans/Wildthing/Wildthing.h
@@ -1,12 +1,16 @@
 #ifndef WILDTHING_H
 #define WILDTHING_H
 #include <Charibot.h>
+#include <WirelessController.h>
 class Wildthing : public Charibot
 {
     public:
         Wildthing();
         virtual void setup() override;
         virtual void loop() override;
+    private:
+        HardwareSerial &xbeeSerial = Serial1;
+        Controller* pWirelessController;
     
 };
 #endif
